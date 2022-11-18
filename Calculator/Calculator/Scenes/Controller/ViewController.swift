ebe403fe0b49c813d756ab5fe96edafef7dcc547
omerfkzr <omer.kazar22@gmail.com>
2022-11-18 19:34:16 +0300

    Project Created, Folder Structure Changed to MVC

---
+++ Calculator/Calculator/Scenes/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Ömer Faruk Kazar on 18.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
